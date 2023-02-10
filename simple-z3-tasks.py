6a39ee6711d875cc1295f9d18ff66663561217b3
Fedor Gromov <71649230+TheTheoden@users.noreply.github.com>
2023-02-10 22:46:42 +0100

    And now it works

--- simple-z3-tasks.py
+++ simple-z3-tasks.py
@@ -14,20 +14,26 @@ def is_prime(n):
 
 def independent_set(list_of_edges, num_nodes):
     solver = Solver()
-    nodes = [Bool("v%d" % i) for i in range(num_nodes)]
+    nodes = [Int("v%d" % i) for i in range(num_nodes)]
     for i, j in list_of_edges:
-        solver.add(Or(Not(nodes[i]), Not(nodes[j])))
+        solver.add(Or(Not(nodes[i] == 1), Not(nodes[j] == 1)))
 
-    if solver.check() == sat:
-        # Get the optimal solution
-        model = solver.model()
-        # Print the vertices that are in the independent set
-        print("Vertices in the maximum independent set:", [i for i in range(num_nodes) if is_true(model[nodes[i]])])
-    else:
-        print("No solution found.")
+    for i in range(num_nodes):
+        solver.add(Or(nodes[i] == 1, nodes[i] == 0))
+
+    model = solver.model
+    for s in range(num_nodes):
+        solver.add(sum(nodes) > s)
+        if solver.check() == sat:
+            model = solver.model()
+
+        else:
+            break
+            #  print("None")
+    print("Maximum independent set:", [i for i in range(num_nodes) if (model[nodes[i]]) == 1])
 
 
 print(is_prime(25))  # False
 print(is_prime(13))  # True
 edges = [(1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6)]
-independent_set(edges, 7)
+independent_set(edges, 7)  # [0, 3, 5, 6]
